aa10d486da6cac001de4cb8c58964184648bec53
SashaTyutyaev <sashatut2002@yandex.ru>
2025-04-07 00:44:03 +0300

    feat(AFI-34) added docker & minor changes

--- src/main/java/afishaBMSTU/user_service/security/SecurityConfig.java
+++ src/main/java/afishaBMSTU/user_service/security/SecurityConfig.java
@@ -7,9 +7,15 @@ import afishaBMSTU.auth_lib.security.internal.InternalTokenService;
 import afishaBMSTU.user_service.model.user.CustomUserDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -29,9 +35,23 @@ public class SecurityConfig extends BaseSecurityConfig {
         return new InternalTokenFilter(internalTokenService());
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://192.168.0.38:*")); // не setAllowedOrigins!
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
     @Override
     protected void configureHttpSecurity(HttpSecurity http) throws Exception {
         http.addFilterBefore(internalTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll()
                 );
